b9bb7376e0b1b20c51d1a3f2af8742bb34576c11
Leo <eta.development.leo@outlook.com>
2024-03-05 10:52:23 +0300

    feat: upload packages on docker volume

--- src/routers/deployment_package_router.py
+++ src/routers/deployment_package_router.py
@@ -1,5 +1,6 @@
 from fastapi import File, HTTPException, Response, UploadFile, status
 from fastapi import APIRouter
+from src.utils.debugging import is_debug
 
 
 router = APIRouter(prefix='/deployments', tags=['Deployments'])
@@ -9,7 +10,7 @@ async def upload_deployments(file: UploadFile = File(...)):
     if file.content_type != 'application/zip':
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='That format filer is not allowed!')
 
-    with open(f'data/deployment/{file.filename}', 'wb') as f:
+    with open(f'data/deployment/{file.filename}' if is_debug() else f'_internal/data/deployment/{file.filename}', 'wb') as f:
         f.write(file.file.read())
 
     return Response(status_code=status.HTTP_200_OK)
