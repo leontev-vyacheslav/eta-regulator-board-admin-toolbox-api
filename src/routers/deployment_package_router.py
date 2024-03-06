a900ef6b3563506a0f6aee383fe533ac987d7af3
Leo <eta.development.leo@outlook.com>
2024-03-06 19:02:58 +0300

    feat: deployment

--- src/routers/deployment_package_router.py
+++ src/routers/deployment_package_router.py
@@ -5,12 +5,17 @@ from src.utils.debugging import is_debug
 
 router = APIRouter(prefix='/deployments', tags=['Deployments'])
 
+
 @router.post('/')
 async def upload_deployments(file: UploadFile = File(...)):
     if file.content_type != 'application/zip':
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='That format filer is not allowed!')
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail='That format filer is not allowed!')
 
-    with open(f'data/deployment/{file.filename}' if is_debug() else f'_internal/data/deployment/{file.filename}', 'wb') as f:
+    with open(f'data/deployment/{file.filename}'
+              if is_debug()
+              else f'_internal/data/deployment/{file.filename}', 'wb'
+              ) as f:
         f.write(file.file.read())
 
     return Response(status_code=status.HTTP_200_OK)
